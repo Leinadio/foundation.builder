daeb4533b8e23861ea83bed2d40c23e92181e40b
Daniel Dupont <danieldupont.contact@gmail.com>
2025-07-02 15:07:32 +0200

    add supabase

--- src/lib/di-container.ts
+++ src/lib/di-container.ts
@@ -1,6 +1,7 @@
 import { UserService } from "../core/services/user.service";
 import { UserPortIn } from "../core/ports/in/user.port";
 import { FirestoreUserRepositoryImpl } from "@/repositories/firestore.user.repository.impl";
+// import { SupabaseUserRepositoryImpl } from "@/repositories/supabase.user.repository.impl";
 import { AuthService } from "@/core/services/auth.service";
 import { AuthPortIn } from "@/core/ports/in/auth.port";
 import { FirebaseAuthRepositoryImpl } from "@/repositories/firebase.auth.repository.impl";
