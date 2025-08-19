a8c2af36922ef5f89dbc779bcd87e87231b0d57e
Daniel Dupont <danieldupont.contact@gmail.com>
2025-08-19 10:33:10 +0200

    limit the acces to /app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,19 @@
+import { auth } from "@/lib/better-auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 export default async function App() {
-  return <div>App</div>;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/");
+  }
+
+  return (
+    <div>
+      <h1>Welcome {session.user.name}</h1>
+    </div>
+  );
 }
