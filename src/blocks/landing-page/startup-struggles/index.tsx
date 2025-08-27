ad3c2a77b64b312015850ca09d3528251513b204
Daniel Dupont <danieldupont.contact@gmail.com>
2025-08-27 17:30:33 +0200

    Removed the `StartupStruggles` component

--- src/blocks/landing-page/startup-struggles/index.tsx
+++
@@ -1,5 +0,0 @@
-import { StartupStruggles, StartupStrugglesProps } from "@/blocks/landing-page/startup-struggles/ui";
-
-export function StartupStrugglesContainer(props: StartupStrugglesProps) {
-  return <StartupStruggles {...props} />;
-}
