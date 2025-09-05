ff13f3da6c388b49db10ce96f9219edc4d4caa5a
Daniel Dupont <danieldupont.contact@gmail.com>
2025-09-05 23:54:44 +0200

    Added new images for the documentation on the hexagonal architecture and the difference between client and server. - Updated the comments in the `page.tsx` file to clarify the code example. - Created new documentation files on Firebase configuration and the hexagonal architecture, as well as input and output ports. - Improved the structure of the documentation with detailed sections and code examples.

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,8 +1,17 @@
 // .source folder will be generated when you run `next dev`
 import { docs } from "../../.source";
 import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement } from "react";
 
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
