71aaeddd16d53f4d65d433f4ae61f4f5796ed3db
Daniel Dupont <danieldupont.contact@gmail.com>
2025-09-05 15:15:13 +0200

    add a first documentation

--- .source/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_0 from "../src/content/docs/index.mdx?collection=docs&hash=1757011323169"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/danieldupont/Developer/foundation.builder/src/content/docs/index.mdx"}, data: docs_0 }], [])
